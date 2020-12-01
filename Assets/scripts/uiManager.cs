894c4f3ef7a43ee8b6011cd760d665f28f1398c5
tuckers18 <506932@chrome.escg.ac.uk>
2020-12-01 11:20:43 +0000

    menu made

--- Assets/scripts/uiManager.cs
+++ Assets/scripts/uiManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class uiManager : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,14 @@ public class uiManager : MonoBehaviour
     {
 
     }
+
+    public void play()
+    {
+        SceneManager.LoadScene("Level1");
+          
+    }
+
+
     public void Pause()
     {
 
